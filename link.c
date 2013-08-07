9e23679b7de438a8aa06c643dc79d645b9c75142
sunnylost <sunnylost@gmail.com>
2013-08-07 08:46:24 +0800

    双链表插入

--- link.c
+++ link.c
@@ -4,12 +4,13 @@
 #define FALSE 0
 #define TRUE  1
 
+/*
 typedef struct NODE {
     struct NODE *link;
     int value;
 } Node;
 
-int insert(Node **linkp, int new_value) {
+int sll_insert(Node **linkp, int new_value) {
     register Node *current;
     register Node *new_Node;
      
@@ -24,6 +25,31 @@ int insert(Node **linkp, int new_value) {
     new_Node->link = current;
     *linkp = new_Node;
     return TRUE;
+}
+*/
+
+typedef struct NODE {
+    NODE *fwd;
+    NODE *bwd;
+    int value;
+} Node;
+
+int dll_insert(Node *rootp, int value) {
+    Node *this_node, *next_node, *new_node;
+    for(this_node = rootp; (next_node = this_node->fwd) != NULL; this_node = next_node) {
+        if(next_node->value == value) return 0;
+        if(next_node->value > value) break;
+    }
+    new_node = (Node *)malloc(sizeof Node);
+    if(new_node == NULL) return -1;
+
+    new_node->value = value;
+
+    new_node->fwd = next_node;
+    this_node->fwd = new_node;
+    new_node->bwd = this_node != rootp ? this_node : NULL;
+    (next_node != NULL ? next_node : rootp)->bwd = new_node;
+    return 1;
 }
 
 int main() {
