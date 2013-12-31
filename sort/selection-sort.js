d33a35046d964d3c8b7e0cb68d80964a829e32c9
sunnylost <sunnylost@gmail.com>
2013-12-31 17:22:31 +0800

    选择排序

---
+++ sort/selection-sort.js
@@ -0,0 +1,27 @@
+var TestSort = require('./test-sort').TestSort;
+
+function selectionSort(a) {
+    var i   = 0,
+        len = a.length - 1,
+        j, min, min_index, tmp;
+
+    for(; i <= len; i++) {
+        min = a[i];
+        min_index = i;
+
+        for(j = i + 1; j <= len; j++) {
+            if(a[j] < min) {
+                min = a[j];
+                min_index = j;
+            }
+        }
+
+        tmp  = a[i];
+        a[i] = a[min_index];
+        a[min_index] = tmp;
+    }
+
+    return a;
+}
+
+TestSort.test(selectionSort);
