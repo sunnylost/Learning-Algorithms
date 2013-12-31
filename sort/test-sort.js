f80f50a5d50ef2e1cd7b5e57258039e08c7618e9
sunnylost <sunnylost@gmail.com>
2013-12-31 17:23:40 +0800

    调整格式

--- sort/test-sort.js
+++ sort/test-sort.js
@@ -1,15 +1,16 @@
 exports.TestSort = {
     randomArray: function() {
         var max  = 100,
-            len = parseInt(Math.random() * 10) + 10,
-            a   = new Array(len);
+            len  = parseInt(Math.random() * 10) + 10,
+            a    = new Array(len);
+            
         for(var i = 0; i < len; i++) {
             a[i] = parseInt(Math.random() * 100);
         }
 
         a.toString = function() {
             return '[' + this.join(',') + ']';
-        }
+        };
 
         return a;
     },
