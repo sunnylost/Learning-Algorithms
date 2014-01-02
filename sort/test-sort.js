c5e3614bb6fa962f96aa6b821740d49bf344c691
sunnylost <sunnylost@gmail.com>
2014-01-02 13:40:20 +0800

    fix bug
    原来的代码里没有给 sort() 传自定义排序函数，导致结果有问题；
    重写 Array 的 toString()；

--- sort/test-sort.js
+++ sort/test-sort.js
@@ -1,16 +1,15 @@
-exports.TestSort = {
+Array.prototype.toString = function() {
+    return '[' + this.join(',') + ']';
+};
+
+TestSort = {
     randomArray: function() {
         var max  = 100,
-            len  = parseInt(Math.random() * 10) + 10,
-            a    = new Array(len);
-            
+            len = parseInt(Math.random() * 10) + 10,
+            a   = new Array(len);
         for(var i = 0; i < len; i++) {
             a[i] = parseInt(Math.random() * 100);
         }
-
-        a.toString = function() {
-            return '[' + this.join(',') + ']';
-        };
 
         return a;
     },
@@ -20,6 +19,6 @@ exports.TestSort = {
             after;
         console.log('Before Sort: '  + a.toString());
         console.log('After Sort: '   + (after = fn(a)));
-        console.log('The result is ' + (after == a.sort()));
+        console.log('The result is ' + (after.toString() == a.sort(function(a, b) { return a - b; }).toString()));
     }
 };
